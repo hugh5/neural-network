5fa615b6c792570e99759ab62a413a717d9e07cc
hugh5 <hughdrummond11@gmail.com>
2024-04-10 22:58:23 +1000

    Initial Commit

---
+++ neural-network/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  neural-network
+//
+//  Created by Hugh Drummond on 10/4/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
